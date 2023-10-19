010dbc9131e8b679dd43ceb9bb9636ce2fe6b72f
ydhong_hit <42939497+ydhongHIT@users.noreply.github.com>
2023-10-19 22:48:47 +0800

    Delete mmseg/.mim directory

--- mmseg/.mim/configs/_base_/schedules/schedule_80k_adamw_fine_decay.py
+++
@@ -1,9 +0,0 @@
-# optimizer
-optimizer = dict(type='AdamW', lr=0.00006, weight_decay=0.01)
-optimizer_config = dict()
-# learning policy
-lr_config = dict(policy='poly', power=0.9, min_lr=5e-6, by_epoch=False)
-# runtime settings
-runner = dict(type='IterBasedRunner', max_iters=80000)
-checkpoint_config = dict(by_epoch=False, interval=8000)
-evaluation = dict(interval=8000, metric='mIoU')
